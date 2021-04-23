af4a63863063b5fee0cff70fff02a0f31d631ea0
youngkvng <francischima168@gmail.com>
2021-04-23 15:20:42 +0100

    master

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="keyword" content="task adder">
+    <meta name="description" content="add your tasks to store for future">
 <!-- css cdn -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 <!-- fontawesome cdn -->
